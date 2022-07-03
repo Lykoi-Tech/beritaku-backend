8357745c328c8e752fa7b940bed3f7408efb9b41
rafiudd <rayhanrafiudd@gmail.com>
2022-07-03 13:10:40 +0700

    Init: Initial Repository

---
+++ routes/route.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const apiHandlers = require('../modules/api_handler');
+
+router.get('/', function (_, res) {
+  res.json({
+    status: 200,
+    message: 'Service is successfull running :D'
+  });
+});
+
+router.post('/users/register', apiHandlers.registerUser);
+
+module.exports = router;
